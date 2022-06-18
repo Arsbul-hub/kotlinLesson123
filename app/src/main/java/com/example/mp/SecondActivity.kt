92180931e2ddcfb47b0a3a7482eaca2e6a69662f
Calrissian <va2005art@gmail.com>
2022-06-18 16:07:16 +0300

    refactor

--- app/src/main/java/com/example/mp/SecondActivity.kt
+++ app/src/main/java/com/example/mp/SecondActivity.kt
@@ -5,19 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class secondActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second)
         val next: Button = findViewById(R.id.next)
         next.setOnClickListener {
-            val intent = Intent(this@secondActivity, thirdActivity::class.java)
+            val intent = Intent(this@SecondActivity, ThirdActivity::class.java)
             startActivity(intent)
         }
         val back: Button = findViewById(R.id.back)
         back.setOnClickListener {
-            val intent = Intent(this@secondActivity, FirstActivity::class.java)
-            startActivity(intent)
+            finish()
         }
     }
 }
